4ad87f7de9f4b6f56666c9d027d3d07bacae9cb6
koujirou23 <kou23ilike5296@gmail.com>
2020-10-15 11:44:16 +0900

    新規投稿ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,14 @@
 class PostsController < ApplicationController
 
   def index
-    @posts == Post.all
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
   end
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+
+  validates :title, :text, presence: true
+
+  belongs_to :user
+  has_many :comments
+  has_many :posts_tags
+  has_many :tags, through: :posts_tags
 end
