cf8ad65ab9547dda711ae88aaeb4a7a6a8163797
koujirou23 <kou23ilike5296@gmail.com>
2020-10-19 17:36:20 +0900

    投稿内容編集機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show,:edit,:destroy]
+  before_action :set_post, only: [:show,:edit,:update,:destroy]
   
   def index
     @posts = Post.all.order(id: 'DESC')
@@ -27,6 +27,14 @@ class PostsController < ApplicationController
   def edit
   end
 
+  def update
+    if @post.update(update_params)
+      redirect_to post_path
+    else 
+      render :edit
+    end
+  end
+
   def destroy
     if @post.destroy
     redirect_to root_path
@@ -39,6 +47,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:posts_tag).permit(:image, :title, :text, :name).merge(user_id: current_user.id)
+  end
+
+  def update_params
+    params.require(:post).permit(:image, :title, :text)
   end
 
   def set_post
