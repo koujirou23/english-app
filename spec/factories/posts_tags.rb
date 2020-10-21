5434ba76957dae21baeefa215e7fd000d267cf64
koujirou23 <kou23ilike5296@gmail.com>
2020-10-21 17:05:53 +0900

    出品のモジュール作成

--- spec/factories/posts_tags.rb
+++ spec/factories/posts_tags.rb
@@ -3,9 +3,5 @@ FactoryBot.define do
     title     { 'title' }
     text      { 'text' }
     name      { 'tag' }
-    user_id   { 1 }
-    # after(:build) do |post|
-    #   post.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
-    # end
   end
 end
